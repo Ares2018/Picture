1d72d3f94189d68020793a48acdf2ce1d5a103e4
a_liYa <liyaf@8531.cn>
2018-01-09 15:00:30 +0800

    初始化项目

---
+++ app/src/main/java/com/core/glide/simple/MainActivity.java
@@ -0,0 +1,13 @@
+package com.core.glide.simple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
