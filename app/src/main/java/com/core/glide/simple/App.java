f1089f9a124c6d570053433c077f0307cf722247
a_liYa <liyaf@8531.cn>
2018-01-19 11:19:26 +0800

    添加Application

---
+++ app/src/main/java/com/core/glide/simple/App.java
@@ -0,0 +1,18 @@
+package com.core.glide.simple;
+
+import android.app.Application;
+
+/**
+ * App
+ *
+ * @author a_liYa
+ * @date 2018/1/19 10:58.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
